2f77d7bd60a41ce17d53a3cb53532098f6232cc7
Patrick 'smauls' McMenomy <pat.mcmenomy@me.com>
2021-10-02 22:06:45 -0600

    Initial Commit

--- TextTransformer/AppDelegate.swift
+++ TextTransformer/AppDelegate.swift
@@ -10,11 +10,13 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "𝑎"
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(showSettings)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -25,6 +27,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true
     }
 
+    @objc func showSettings() {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController")
+                as? ViewController else {
+                    fatalError("Unable to find ViewController in the storyboard.")
+                }
+        
+        guard let button = statusItem.button else {
+            fatalError("Couldn't find status item button")
+        }
+        
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: button.bounds, of: button, preferredEdge: .maxY)
+    }
 
 }
 
--- TextTransformer/ViewController.swift
+++ TextTransformer/ViewController.swift
@@ -2,18 +2,22 @@
 //  ViewController.swift
 //  TextTransformer
 //
+//  Following tutorial by HackingwithSwift.com
+//
 //  Created by Patrick McMenomy on 9/27/21.
 //
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTextFieldDelegate {
+    @IBOutlet weak var input: NSTextField!
+    @IBOutlet weak var type: NSSegmentedControl!
+    @IBOutlet weak var output: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        typeChanged(self)    }
 
     override var representedObject: Any? {
         didSet {
@@ -21,6 +25,65 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func typeChanged(_ sender: Any) {
+        switch type.selectedSegment{
+        case 0:
+            output.stringValue = rot13(input.stringValue)
+        case 1:
+            output.stringValue = similar(input.stringValue)
+        case 2:
+            output.stringValue = strike(input.stringValue)
+        default:
+            output.stringValue = zalgo(input.stringValue)
+        }
+    }
+    
+    
+    @IBAction func copyToPasteboard(_ sender: Any) {
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(output.stringValue, forType: .string)
+    }
+    
+    func controlTextDidChange(_ obj: Notification) {
+        typeChanged(self)
+    }
+    
+    func rot13(_ input: String) -> String {
+        return "ROT13: " + input
+    }
+    
+    func similar(_ input: String) -> String {
+        var output = input
+        
+        output = output.replacingOccurrences(of: "a", with: "а")
+        output = output.replacingOccurrences(of: "e", with: "е")
+        output = output.replacingOccurrences(of: "i", with: "і")
+        output = output.replacingOccurrences(of: "o", with: "о")
+        
+        output = output.replacingOccurrences(of: "A", with: "А")
+        output = output.replacingOccurrences(of: "E", with: "Е")
+        output = output.replacingOccurrences(of: "I", with: "І")
+        output = output.replacingOccurrences(of: "O", with: "О")
+        
+        return output
+    }
+    
+    func strike(_ input: String) -> String {
+        var output = ""
+        
+        for  letter in input {
+            output.append(letter)
+            
+            output.append("\u{0335}")
+        }
+        
+        return output
+        
+    }
+    
+    func zalgo(_ input: String) -> String {
+        return "Zalgo: " + input
+    }
+    
 }
 
